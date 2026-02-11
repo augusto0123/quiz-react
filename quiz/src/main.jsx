d52c412c81aacccb575278692345838b6a118c0e
Augusto <senescal.oliveira@gmail.com>
2026-02-10 21:32:04 -0300

    feat: configurando o reducer

--- quiz/src/main.jsx
+++ quiz/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactDOM from "react-dom/client"
 
 import { QuizProvider } from './context/quiz.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QuizProvider>
       <App />
