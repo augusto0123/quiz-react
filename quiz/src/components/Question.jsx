69e240259c7891bff193f4761a4df32217e39bc1
Augusto <senescal.oliveira@gmail.com>
2026-02-11 22:17:31 -0300

    feat: criacao da funcionalidade de dica

--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -31,6 +31,19 @@ const Question = () => {
               selectOption={() => onSelectOption(option)} />
             ))}
         </div>
+
+        {!quizState.answerSelected && !quizState.help && (
+          <>
+          {currentQuestion.tip && (
+            <button onClick={() => dispatch({type: "SHOW_TIP"})}>Dica</button>
+          )}
+
+          </>
+        )}
+
+        {!quizState.answerSelected && quizState.help === "tip" && <p>{currentQuestion.tip}</p>}
+
+
         {quizState.answerSelected && (
           <button onClick={() => dispatch({type: "CHANGE_QUESTION"})}>
           Continuar
